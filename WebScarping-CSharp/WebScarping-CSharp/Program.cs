4fa79787998b99d581653bb483a7499559c978b2
Ghyles <34336058+Ghyles@users.noreply.github.com>
2019-02-28 13:51:04 +0100

    Add files via upload

---
+++ WebScarping-CSharp/WebScarping-CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScarping_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Web-Scarping | Aghyles OUGHANEM");
+
+            Console.ReadKey();
+        }
+    }
+}
